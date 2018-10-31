7552fed43d2baaf0b2634387a7775ca5fedf0095
Alcides <cidesjour13@gmail.com>
2018-10-30 22:36:47 -0300

    Alterações em fila.py

--- fila.py
+++ fila.py
@@ -10,24 +10,45 @@ class Queue:
 
         '''Verificar se a fila esta vazia'''
 
+        return self.__head == None
+
 
     def enqueue(self, *item):
 
+        current = self.__head
         temp = PokeNode(*item)
-        current = self.__head        
-
-        '''Colocar item na posição -1'''
+        #A lista está vazia
+        if current == None:
+            self.__head = temp
+        #A lista não está vazia
+        else:
+            while current.get_next() != None:
+                current = current.get_next()
+            current.set_next(temp)
 
 
     def dequeue(self):
         
         '''Retirar item na posição 0'''
 
+        current = self.__head
+        dq = current.get_data()
+        self.__head = current.get_next()
+
+        return dq
+
 
     def size(self):
 
-        '''Colocar item na posição -1'''
+        '''Retorna a quantidade de itens presentes na fila'''
+    
+    count = 0
+    current = self.__head
+    while current != None:
+        count += 1
+        current = current.get_next()
 
+    return count
 
     def __str__(self):
 
